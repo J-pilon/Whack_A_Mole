de54cfb914edb101b1377fcfa5d4013ec5ef0021
Josiah <josiah.pilon@yahoo.ca>
2021-06-24 13:54:28 -0700

    initialized express and listening on port 3000

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.listen('localhost', )
+app.get('/', (req, res) => {
+  res.sendFile("./index.html");
+})
+
+app.listen(3000, 'localhost', () => {
+  console.log('Server is running on port 3000');
+})
